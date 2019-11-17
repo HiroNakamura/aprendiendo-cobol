daeb5c429cd3936651638e40bc8c022dfb88f20e
Fer_Carraro <carraro.fernando@gmail.com>
2019-11-17 16:59:01 -0600

    Se ha editado estructura.cbl

--- temario/chapter1/estructura.cbl
+++ temario/chapter1/estructura.cbl
@@ -21,8 +21,14 @@ WORKING-STORAGE SECTION.
    03 Nombre PIC A(33) VALUE SPACES.
  02 Genero PIC A(1).
  02 Edad PIC 9(2) VALUE ZEROS.
+01 Datos.
+ 02 Numero PIC 9(3)V99 VALUE 456.09.
+ 02 Modelo.
+   03 Marca PIC A(20) VALUE "WHIRPOOL".
+   03 Precio PIC 9(4)V99 VALUE 3400.00.
+   
 
-
+*> main del programa
 PROCEDURE DIVISION.
  MOVE 67094 TO EstudianteId.
  MOVE "DSC" TO Iniciales.
@@ -30,7 +36,21 @@ PROCEDURE DIVISION.
  MOVE "M" TO Genero.
  MOVE 32 TO Edad.
  DISPLAY "[ "Estudiante" ]".
+ *> SIMILAR A MOVE
  SET Edad TO 33.
+ SET Genero TO "G".
+ SET EstudianteId TO 3400210.
+ SET Iniciales TO "JAO".
+ SET Nombre TO "JULIA ALVAREZ OLMEDO".
  DISPLAY "[ "Estudiante" ]".
+ DISPLAY "[ Estudiante ]".
+ DISPLAY "Nombre: "Nombre.
+ DISPLAY "----------------".
+ DISPLAY "[ "Datos" ]".
+ SET Marca TO "UNILEVER".
+ SET Precio TO 2500.
+ DISPLAY "[ "Datos" ]".
+ DISPLAY "Marca: "Marca.
+ DISPLAY "Precio: $"Precio.
 
 EXIT PROGRAM.
